7647f9be02d2ea4d872ad094282f3ac06a0720be
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2019-09-07 03:19:04 +0200

    Add more chunk test cases

--- __tests__/array/chunk.test.js
+++ __tests__/array/chunk.test.js
@@ -12,6 +12,16 @@ test('returns chunked array', () => {
   expect(result).toEqual([[2, 4], [6, 7], ['john']]);
 });
 
+test('returns empty array if size is 0', () => {
+  result = chunk(testArray, 0);
+  expect(result).toEqual([]);
+});
+
+test('returns the empty array if size is negative', () => {
+  result = chunk(testArray, -5);
+  expect(result).toEqual([]);
+});
+
 test('does not mutate the original array', () => {
   expect(testArray.length).toBe(5);
 });
