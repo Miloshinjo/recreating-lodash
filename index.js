a56d154f65ac2ad699424a22dd172222cb7fe64b
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2019-10-01 14:35:06 +0200

    Arrange starter code

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-import _ from 'lodash';
-
-import { numberArray } from './__data__/arrays';
-import chunk from './array/chunk';
-
-console.log('lodash chunk', _.chunk(numberArray));
-console.log('custom chunk', chunk(numberArray));
+console.log('hello lodash');
