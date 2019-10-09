e15cfc10b2302fba9775cfca57f017e6b7d2e2cc
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2019-10-09 20:13:10 +0200

    Add starter data

---
+++ __data__/arrays.js
@@ -0,0 +1,21 @@
+export const randomArray = [
+  'John Smith',
+  NaN,
+  '',
+  'Jane Smith',
+  12,
+  { pure: false },
+  false,
+  15,
+  'Adam Smith',
+  undefined,
+  3,
+  'Milos',
+  null,
+  0,
+  -1
+];
+
+export const numberArray = [1, 2, 3, 4, 5, 6, 7];
+export const numberArray2 = [2, 4, 6, 7];
+export const stringArray = ['a', 'b', 'c', 'd', 'e'];
