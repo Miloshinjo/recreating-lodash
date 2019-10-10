3c1fd2def22e874608f40790317467903cdf28fd
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2019-10-10 20:01:04 +0200

    Add compact

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import _ from 'lodash';
 
-import { numberArray } from './__data__/arrays';
 import chunk from './array/chunk';
-
-console.log('lodash chunk', _.chunk(numberArray, 4));
-console.log('custom chunk', chunk(numberArray, 4));
+import compact from './array/compact';
