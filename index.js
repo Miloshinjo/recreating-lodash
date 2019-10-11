b53214698c6da8edcfb8fd28a5e842197ff6ee60
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2019-10-11 13:40:46 +0200

    Edit example

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import chunk from './array/chunk';
 
 export const testArray = [1, 2, 3, 4, 5, 6, 7];
 
-console.log('lodash chunk', _.chunk(testArray, 4));
-console.log('custom chunk', chunk(testArray, 4));
+console.log('lodash chunk', _.chunk(testArray, 3));
+console.log('custom chunk', chunk(testArray, 3));
